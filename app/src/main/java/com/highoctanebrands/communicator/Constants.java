573ddb867a4fb17b62f0aa957d4a494b31b7b2d4
Eric Haskins <ehask71@gmail.com>
2014-09-28 23:19:58 -0400

    u

--- app/src/main/java/com/highoctanebrands/communicator/Constants.java
+++ app/src/main/java/com/highoctanebrands/communicator/Constants.java
@@ -3,5 +3,8 @@ package com.highoctanebrands.communicator;
 
 public interface Constants {
     String LOG = "Communicator";
+    //  Server Key
+    //  AIzaSyA11-8f6j7F2oCUoiXiMHXCmhyr68TNVR4
     String GCMSender = "491428620572";
+
 }
